d95aebfca49f813abbf8815ab505a0a44748b67b
Your Name <you@example.com>
2019-09-16 14:40:29 -0700

    created knexfile

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ const express = require('express');
 const server = express();
 server.use(express.json())
 
+const bcrypt = require('bcryptjs')
+
 const port = process.env.PORT || 5005;
 server.listen(port, ()=> console.log(`Running on port ${port}`))
+
+server.get('/', (req,res)=> {
+    res.send('Ready to code')
+})
+
+server.post('/api/register', (req,res)=> {
+    let {username, password} = req.body; 
+    const hash = bscypt.hashSync(password, 12)
+
+    
+})

8657dafe517e68a6a491b9376e369f112856b40a
Your Name <you@example.com>
2019-09-16 15:08:31 -0700

    got POST request for user login work

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ server.post('/api/register', (req,res)=> {
             res.status(500).json(err)
         })
 })
+
+server.post('/api/login', (req,res)=> {
+    let {username, password} = req.body;
+
+    Users.findBy({username})
+        .first()
+        .then (user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({message: `Logged in`})
+            } else {
+                res.status(401).json({message: 'Invalid username or password'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})

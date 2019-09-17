9f269726a87cf70efb790535e73cd76ec15a0209
Your Name <you@example.com>
2019-09-17 13:04:10 -0700

    used session to restrict user in middleware

--- middleware/restricted-middleware.js
+++ middleware/restricted-middleware.js
@@ -1,20 +1,10 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model')
 
 module.exports = (req,res,next) => {
-    let {username, password} = req.body;
 
-    Users.findBy({username})
-    .first()
-    .then (user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
+    if (req.session && req.session.userinfo) {
             next()
         } else {
-            res.status(401).json({message: 'Incorrect password'})
+            res.status(401).json({message: 'You shall not pass'})
         }
-    })
-    .catch(err => {
-        res.status(500).json({message: 'Incorrect username and password'})
-    })
+    
 }
